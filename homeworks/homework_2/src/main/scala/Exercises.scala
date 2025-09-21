f7446e298685f64b4dde0c04de20f3177d47a1c5
kelwin <kelwin90210@gmail.com>
2025-09-21 17:22:05 +0300

    add functions and tests

--- homeworks/homework_2/src/main/scala/Exercises.scala
+++ homeworks/homework_2/src/main/scala/Exercises.scala
@@ -16,7 +16,15 @@ object Exercises {
     /*Реализовать функцию, которая возвращает сумму всех целых чисел в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 5.*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = ???
+    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = {
+        var count: Long = 0;
+        for ( i <- iFrom to iTo ) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                count += i
+            }
+        }
+        count
+    }
 
 
 
@@ -25,8 +33,19 @@ object Exercises {
     Число 80 раскладывается на множители 1 * 2 * 2 * 2 * 2 * 5, результат выполнения функции => Seq(2, 5).
     Число 98 можно разложить на множители 1 * 2 * 7 * 7, результат выполнения функции => Seq(2, 7).*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def primeFactor(number: Int): Seq[Int] = ???
-
+    def primeFactor(number: Int): Seq[Int] = number match {
+        case n if n <= 1 => Seq.empty
+        case n => 
+            @annotation.tailrec
+            def factors(num: Int, div: Int, acc: Set[Int]): Set[Int] = (num, div) match {
+                case (x, _) if x <= 1 => acc
+                case (x, d) if d * d > x => if (x > 1) acc + x else acc
+                case (x, d) if x % d == 0 => factors(x / d, d, acc + d)
+                case (x, d) => factors(x, d + 1, acc)
+            }
+    
+        factors(n, 2, Set.empty).toSeq.sorted
+}
 
 
     /*ЗАДАНИЕ III*/
@@ -40,15 +59,12 @@ object Exercises {
     def abs(vec: Vector2D): Double = java.lang.Math.sqrt(vec.x * vec.x + vec.y * vec.y)
     def scalar(vec0: Vector2D, vec1: Vector2D): Double = vec0.x * vec1.x + vec0.y * vec1.y
     def cosBetween(vec0: Vector2D, vec1: Vector2D): Double = scalar(vec0, vec1) / abs(vec0) / abs(vec1)
-    //def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, ???, rightVec0: Vector2D, rightVec1: Vector2D) = ???
-    /*
+    def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, func: (Vector2D, Vector2D) => Double, rightVec0: Vector2D, rightVec1: Vector2D) = func(leftVec0, leftVec1) + func(rightVec0, rightVec1)
+    
     def sumScalars(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, scalar, rightVec0, rightVec1)
-    */
-    /*
     def sumCosines(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, cosBetween, rightVec0, rightVec1)
-    */
 
 
 
@@ -71,6 +87,13 @@ object Exercises {
             "Chrome" ->   (3,   7.18),   "Cesium" ->    (7,   1.873), "Zirconium" -> (3,   6.45)
         )
 
-    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = ???
+    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = {
+        def getWeight(radius: Int, density: Double): Double = {
+            (4.0 / 3.0) * java.lang.Math.PI * java.lang.Math.pow(radius, 3) * density
+        }
+        ballsArray.toSeq
+          .sortBy{ case ( _, (radius, density)) => getWeight(radius, density) }
+          .map { case (name, _) => name }
+    }
 
 }
--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -1,4 +1,5 @@
 import utest._
+import Exercises.Vector2D
 
 object Test extends TestSuite{
 
@@ -7,6 +8,40 @@ object Test extends TestSuite{
             assert(Exercises.divBy3Or7(1, 3) == Seq(3))
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
             assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
+        }
+        'test_sumOfDivBy30r5 - {
+            assert(Exercises.sumOfDivBy3Or5(1, 5) == 8)
+            assert(Exercises.sumOfDivBy3Or5(-5, 5) == 0)
+            assert(Exercises.sumOfDivBy3Or5(6, 6) == 6)
+            assert(Exercises.sumOfDivBy3Or5(4, 4) == 0)
+        }
+        'test_primeFactor - {
+            assert(Exercises.primeFactor(80) == Seq(2, 5))
+            assert(Exercises.primeFactor(98) == Seq(2, 7))
+            assert(Exercises.primeFactor(13) == Seq(13))
+            assert(Exercises.primeFactor(210) == Seq(2,3,5,7))
+            assert(Exercises.primeFactor((1024)) == Seq(2))
+            assert(Exercises.primeFactor(101 * 103) == Seq(101, 103))
+        }
+        'test_sumScalars - {
+            assert(Exercises.sumScalars(Vector2D(1, 0), Vector2D(0, 1), Vector2D(2, 0), Vector2D(3, 0)) == 6.0)
+            assert(Exercises.sumScalars(Vector2D(1, 0), Vector2D(0, 1), Vector2D(0, 1), Vector2D(1, 0)) == 0.0)
+            assert(Exercises.sumScalars(Vector2D(2, 0), Vector2D(3, 0), Vector2D(4, 0), Vector2D(5, 0)) == 26.0)
+        }
+        'test_sumCosines - {
+            assert(Exercises.sumCosines(Vector2D(1, 0), Vector2D(1, 0), Vector2D(2, 0), Vector2D(3, 0)) == 2.0)
+            assert(Exercises.sumCosines(Vector2D(1, 0), Vector2D(0, 1), Vector2D(2, 0), Vector2D(0, 3)) == 0.0)
+            assert(Exercises.sumCosines(Vector2D(1, 0), Vector2D(-1, 0), Vector2D(2, 0), Vector2D(-3, 0)) == -2.0)
+        }
+        'test_sortByHeavyweight - {
+            assert(Exercises.sortByHeavyweight() == Seq("Tin", "Platinum", "Nickel", "Aluminum", "Titanium", "Lead", "Sodium", "Uranium", "Gold", "Tungsten", "Zirconium", "Chrome", "Iron", "Copper", "Silver", "Plutonium", "Cobalt", "Cesium", "Calcium", "Lithium", "Magnesium", "Potassium", "Graphite"))
+            assert(Exercises.sortByHeavyweight(Map(
+                "Gold" -> (2,   19.32), "Titanium" -> (2,   10.50), "Graphite" -> (12, 2.1)))
+              == Seq("Titanium", "Gold", "Graphite"))
+
+            assert(Exercises.sortByHeavyweight(Map(
+                "Chrome" -> (3,   7.18),"Uranium" -> (2,   19.04), "Cesium" -> (7,   1.873)))
+              == Seq( "Uranium", "Chrome", "Cesium"))
         }
     }
 }
